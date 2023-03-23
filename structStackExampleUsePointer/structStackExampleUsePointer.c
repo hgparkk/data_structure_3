58ef955353934bbdad39d80753cdcb300ec90787
hgparkk <phg2559@naver.com>
2023-03-23 11:43:31 +0900

    new commit

--- structStackExampleUsePointer/structStackExampleUsePointer.c
+++ structStackExampleUsePointer/structStackExampleUsePointer.c
@@ -29,37 +29,37 @@ int is_empty(Stack*s) { return s->top == -1; }
 int is_full(Stack*s) { return s->top == MAX_STACK_SIZE; }
 int size(Stack*s) { return s->top + 1; }
 
-void push(Element e, stack* s)
+void push(Stack* s,Element e)
 {
 	if (is_full(s))
 		error("스택 포화 에러");
 	s->data[++s->top] = e;
 }
 
-Element pop()
+Element pop(Stack* s)
 {
-	if (is_empty())
+	if (is_empty(s))
 		error("스택 포화 에러");
-	return data[top--];
+	return s->data[s->top--];
 }
 
-Element peek()
+Element peek(Stack* s)
 {
-	if (is_empty())
+	if (is_empty(s))
 		error("스택 공백 에러");
-	return data[top];
+	return s->data[s->top];
 }
 
-void print_stack(char msg[])
+void print_stack(Stack s, char msg[])
 {
 	int i;
-	printf("%s[%2d]=", msg, size());
-	for (i = 0;i < size();i++)
-		printf("\n\t%-15d %-10s %-20s", data[i].id, data[i].name, data[i].dept);
+	printf("%s[%2d]=", msg, size(&s));
+	for (i = 0;i < size(&s);i++)
+		printf("\n\t%-15d %-10s %-20s", s.data[i].id, s.data[i].name, s.data[i].dept);
 	printf("\n");
 }
 
-Student get_student(int id, char* name, char* dept)
+Student get_student(int id, char name[], char dept[])
 {
 	Student s;
 	s.id = id;
@@ -81,6 +81,7 @@ int main()
 	pop(&s);
 	print_stack(s,"친구 1명 삭제 후");
 	e = peek(&s);
-	printf("ID = %d, Name =%s, Dept, Name = %s \n", e.id, e.name, e.dept);
+	printf("ID = %d, Name =%s, Dept= %s \n", e.id, e.name, e.dept);
+	print_stack(s, "After peek()");
 	return 0;
 }
